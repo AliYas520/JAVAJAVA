724232ccbf30bd7fff8e2fb9bb8e515af4186ece
AliYa <enderguy520@gmail.com>
2025-08-06 11:27:24 +0700

    finished bab 5

--- Java_Bab5/src/java_bab5/Java_Bab5.java
+++ Java_Bab5/src/java_bab5/Java_Bab5.java
@@ -10,53 +10,103 @@ package java_bab5;
  */
 public class Java_Bab5 {
 
-     public static void main(String[] args) {
-//    //Increment (++n)
+    public static void main(String[] args) {
+//
+//        //Increment (++n)
 //        Integer sebelum = 20;
 //        Integer sesudah = 0;
-//        
+//
 //        System.out.println("Sebelum Increment : " + sebelum);
 //        sesudah = ++sebelum;
 //        System.out.println("Setelah Increment : " + sesudah);
-    
-    
-//    //Decrement (--n)
-//        Integer sebelum = 9 ;
+//
+//        //Decrement (--n)
+//        Integer sebelum = 9;
 //        Integer sesudah = 0;
-//        
+//
 //        System.out.println("Sebelum Decrement : " + sebelum);
 //        sesudah = --sebelum;
 //        System.out.println("Setelah Increment : " + sesudah);
-
-
-//    //Negate (-)
-//    int a = 2;
-//    int b = 0;
-//    
-//    System.out.println("Sebelum Negasi : " + a);
-//    b = -a;
-//    System.out.println("Setelah  Negasi : " + b);
-    
-    //penambahan
-    int a1 = 10;
-    int a2 = 2;
-    System.out.println("Penambahan 10 + 2 : " + (a1+a2));
-    //pengurangan
-    int b1 = 10;
-    int b2 = 2;
-    System.out.println("Pengurrangann 10 - 2 : " + (b1-b2));
-    //perkalian
-    int c1 = 10;
-    int c2 = 2;
-    System.out.println("Perkalian 10 * 2 : " + (c1*c2));
-    //pembagian
-    int d1 = 16;
-    int d2 = 2;
-    System.out.println("Pembagian 16 / 2 : " + (d1/d2));
-    //Sisa Bagi
-    int e1 = 16;
-    int e2 = 2;
-    System.out.println("Sisa bagi 16 dan 2 : " + (e1%e2));
+//
+//        //Negate (-)
+//        int a = 2;
+//        int b = 0;
+//
+//        System.out.println("Sebelum Negasi : " + a);
+//        b = -a;
+//        System.out.println("Setelah  Negasi : " + b);
+//
+////// Math Operation  //////
+//        //penambahan
+//        int a1 = 10;
+//        int a2 = 2;
+//        System.out.println("Penambahan 10 + 2 : " + (a1 + a2));
+//        //pengurangan
+//        int b1 = 10;
+//        int b2 = 2;
+//        System.out.println("Pengurrangann 10 - 2 : " + (b1 - b2));
+//        //perkalian
+//        int c1 = 10;
+//        int c2 = 2;
+//        System.out.println("Perkalian 10 * 2 : " + (c1 * c2));
+//        //pembagian
+//        int d1 = 16;
+//        int d2 = 2;
+//        System.out.println("Pembagian 16 / 2 : " + (d1 / d2));
+//        //Sisa Bagi
+//        int e1 = 16;
+//        int e2 = 2;
+//        System.out.println("Sisa bagi 16 dan 2 : " + (e1 % e2));
+//
+////// Logic Operation //////
+//
+//    //Less Than (<) 
+//        int a = 10;
+//        int b = 15;
+//        System.out.println("Variabel a : " + a);
+//        System.out.println("Variabel b ; " + b);
+//        System.out.println(" Apakah 10 kurang dari 15? : " + (a < b));
+//        System.out.println("Apakah 15 kurang dari 10? : " + (b < a));
+//        System.out.println("Apakah 15 kurang dari 15? : " + (b < b));
+//
+//    //More Than (>)
+//        int a = 2;
+//        int b = 3;
+//        System.out.println("Variabel a : " + a);
+//        System.out.println("Variabel b ; " + b);
+//        System.out.println("Apakah 2 lebih besar dari 3? : " + (a > b));
+//        System.out.println("Apakah 3 lebih besar dari 2? : " + (b > a));
+//        System.out.println("Apakah 3 lebih besar dari 3? : " + (b > b));
+//
+//    //Equal or less than (<=)
+//        int a  = 3;
+//        int b = 3;
+//        System.out.println("Variabel a : " + a);
+//        System.out.println("Variabel b : " + b);
+//        System.out.println("Apakah 3 lebih kecil atau sama dengan 3 : " + (a<=b));
+//        a=9;
+//        System.out.println("Variabel a diubah : " + a);
+//        System.out.println("Apakah 9 lebih kecil atau sama dengan 3 : " + (a <= b));
+//
+//    //equal or more than (>=)
+//        int a = 3;
+//        int b = 3;
+//        System.out.println("Variabel a : " + a);
+//        System.out.println("Variabel b : " + b);
+//        System.out.println("Apakah 3 lebih  besar au sama dengan 3 : " + (a >= b));
+//        a = 2;
+//        System.out.println("Variabel a diubah : " + a);
+//        System.out.println("Apakah 2 lebih besar atau sama dengan 3 :" + (a >= b));
+//
+//      //equal and/or not equal (== / != )
+//            int a = 3;
+//            int b = 3;
+//            System.out.println("Variabel a : " + a);
+//            System.out.println("Variabel b : " + b);
+//            System.out.println("Apakah 3 sama dengan 3 : " + (a == b));
+//            a = 2;
+//            System.out.println("Apakah 2 sama dengan 3 : " + (a == b));
+//            System.out.println("Apakah 2 tidak sama  dengan 3 : " + (a != b));
     }
-    
+
 }
