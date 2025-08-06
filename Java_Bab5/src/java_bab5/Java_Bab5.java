c095615331c9a9336fb7e35dc7fd7bb88172bd3a
AliYa <enderguy520@gmail.com>
2025-08-06 10:42:59 +0700

    update

--- Java_Bab5/src/java_bab5/Java_Bab5.java
+++ Java_Bab5/src/java_bab5/Java_Bab5.java
@@ -20,11 +20,43 @@ public class Java_Bab5 {
 //        System.out.println("Setelah Increment : " + sesudah);
     
     
-    //Decrement (--n)
-        Integer sebelum = 9 ;
-        Integer sesudah = 0;
-        
-        
+//    //Decrement (--n)
+//        Integer sebelum = 9 ;
+//        Integer sesudah = 0;
+//        
+//        System.out.println("Sebelum Decrement : " + sebelum);
+//        sesudah = --sebelum;
+//        System.out.println("Setelah Increment : " + sesudah);
+
+
+//    //Negate (-)
+//    int a = 2;
+//    int b = 0;
+//    
+//    System.out.println("Sebelum Negasi : " + a);
+//    b = -a;
+//    System.out.println("Setelah  Negasi : " + b);
+    
+    //penambahan
+    int a1 = 10;
+    int a2 = 2;
+    System.out.println("Penambahan 10 + 2 : " + (a1+a2));
+    //pengurangan
+    int b1 = 10;
+    int b2 = 2;
+    System.out.println("Pengurrangann 10 - 2 : " + (b1-b2));
+    //perkalian
+    int c1 = 10;
+    int c2 = 2;
+    System.out.println("Perkalian 10 * 2 : " + (c1*c2));
+    //pembagian
+    int d1 = 16;
+    int d2 = 2;
+    System.out.println("Pembagian 16 / 2 : " + (d1/d2));
+    //Sisa Bagi
+    int e1 = 16;
+    int e2 = 2;
+    System.out.println("Sisa bagi 16 dan 2 : " + (e1%e2));
     }
     
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ learned java today
 
 #new java project (Bab 3)
 To go to the code, go to:
-Project_Baru/src/projek_baru/projek_baru.java
+Java_Bab3/project_Baru/src/projek_baru/projek_baru.java
 
 #Bab 4
 Java_Bab4/src/java_bab4/java_bab4.java
