0b9ab273beca13c5113eef4b6d999b2d204ed121
AliYa <enderguy520@gmail.com>
2025-08-20 09:24:25 +0700

    Update Java_Bab8.java

--- Java_Bab8/src/java_bab8/Java_Bab8.java
+++ Java_Bab8/src/java_bab8/Java_Bab8.java
@@ -31,23 +31,52 @@ public class Java_Bab8 {
 //                System.out.print("* ");
 //            }
 //            System.out.println(" ");
+//       }
+//        //User input
+//        int JumlahKelas = Integer.parseInt(JOptionPane.showInputDialog(null, "Masukan jumlah kelas : ", "Input", JOptionPane.INFORMATION_MESSAGE));
+//        String IsiKelas[][] = new String[JumlahKelas][];
+//
+//        for (int kelas = 0; kelas < JumlahKelas; kelas++) {
+//            String namaKelas = JOptionPane.showInputDialog(null, "Nama Kelas : ", "Masukan Nama Kelas", JOptionPane.INFORMATION_MESSAGE);
+//            int jumlahKursi = Integer.parseInt(JOptionPane.showInputDialog(null, "Jumlah murid masing-masing kelas : ", "Input", JOptionPane.INFORMATION_MESSAGE));
+//            IsiKelas[kelas] = new String[jumlahKursi];
+//            System.out.println("Kelas " + namaKelas + " : ");
+//            for (int kursi = 0; kursi < jumlahKursi; kursi++) {
+//                String namaSiswa = JOptionPane.showInputDialog(null, "Nama Siswa ", "Kelas " + namaKelas, JOptionPane.INFORMATION_MESSAGE);
+//                IsiKelas[kelas][kursi] = namaSiswa;
+//                System.out.println("Nama Siswa : " + IsiKelas[kelas][kursi]);
+//            }
+//            System.out.println("----------------------------------------------");
+//        //While Loop
+//        int arr[] = new int[4];
+//        boolean hasil = true;
+//        arr[0] = 2;
+//        arr[1] = 5;
+//        arr[2] = 7;
+//        arr[3] = 1;
+//        while (hasil == true) {
+//            String input = JOptionPane.showInputDialog(null, "Masukan bilangan bulat : ");
+//            int cekInputan = Integer.parseInt(input);
+//            for (int a  = 0; a < 4; a++) {
+//            if (cekInputan == arr[a]) {
+//                hasil = false;
+//                break;
+//            }else {
+//                hasil = true;
+//            }
 //        }
-        //User input
-        int JumlahKelas = Integer.parseInt(JOptionPane.showInputDialog(null, "Masukan jumlah kelas : ", "Input", JOptionPane.INFORMATION_MESSAGE));
-        String IsiKelas[][] = new String[JumlahKelas][];
-
-        for (int kelas = 0; kelas < JumlahKelas; kelas++) {
-            String namaKelas = JOptionPane.showInputDialog(null, "Nama Kelas : ", "Masukan Nama Kelas", JOptionPane.INFORMATION_MESSAGE);
-            int jumlahKursi = Integer.parseInt(JOptionPane.showInputDialog(null, "Jumlah murid masing-masing kelas : ", "Input", JOptionPane.INFORMATION_MESSAGE));
-            IsiKelas[kelas] = new String[jumlahKursi];
-            System.out.println("Kelas " + namaKelas + " : ");
-            for (int kursi = 0; kursi < jumlahKursi; kursi++) {
-                String namaSiswa = JOptionPane.showInputDialog(null, "Nama Siswa ", "Kelas " + namaKelas, JOptionPane.INFORMATION_MESSAGE);
-                IsiKelas[kelas][kursi] = namaSiswa;
-                System.out.println("Nama Siswa : " + IsiKelas[kelas][kursi]);
-            }
-            System.out.println("----------------------------------------------");
-        }
+//            if (hasil ==true) {
+//                JOptionPane.showMessageDialog(null, "Angka " + cekInputan + " tidak ada di dalam array.");
+//            } else {
+//                JOptionPane.showMessageDialog(null, "Angka " + cekInputan + " ada di dalam array.");
+//            }
+//        }
+//        //Do While Loop
+//        String Input = "";
+//        int cekbil = 0;
+//        do{
+//            Input = JOptionPane.showInputDialog(null, "4 x 3 : ");
+//            cekbil = Integer.parseInt(Input);
+//        }while(cekbil != 12);
     }
-
 }
